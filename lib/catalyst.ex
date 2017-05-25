06006996cdde9a0abbd012ddf549351806bff180
Vyacheslav Savelyev <v.savelyev@rambler-co.ru>
2017-05-25 17:23:00 +0300

    initial commit

---
+++ lib/catalyst.ex
@@ -0,0 +1,18 @@
+defmodule Catalyst do
+  @moduledoc """
+  Documentation for Catalyst
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Catalyst.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/catalyst_test.exs
@@ -0,0 +1,8 @@
+defmodule CatalystTest do
+  use ExUnit.Case
+  doctest Catalyst
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
