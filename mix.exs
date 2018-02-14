2196b08431d2e3966a84805af89fff5392ad37b8
Vyacheslav Savelyev <v.savelyev@rambler-co.ru>
2018-02-14 16:25:39 +0300

    bump version 1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Catalyst.Mixfile do
   def project do
     [
       app: :catalyst,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
